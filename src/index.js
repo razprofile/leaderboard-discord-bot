ba85693710ff1be06094008e68f860fc81aa5702
Raz Ben Haim <benhaim.raz@gmail.com>
2021-01-09 01:21:15 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ client.on('message', async (msg) => {
   let leaderboardTitle;
   let user = await getUser(msg.author.id);
   msg.channel.send(msg.channel.toString());
-  msg.channel.send(msg.channel.toString() === '#leaderboard');
+  msg.channel.send(msg.channel);
+  msg.channel.send(msg.channel === '#leaderboard');
   if (msg.channel.toString() === '#leaderboard') {
     msg.channel.send('lol');
     leaderboardTitle = 'lol';
