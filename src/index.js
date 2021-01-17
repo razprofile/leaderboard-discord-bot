a6f08d854422d766909b1813f5b45f4320ac2e89
Raz Ben Haim <benhaim.raz@gmail.com>
2021-01-16 23:44:54 -0500

    Testing voiceStateUpdate

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
-  const channel = client.channels.cache.find(
-    (channel) => channel.id === '787066512049176579'
-  );
-  channel.send('Joined session');
+  // const channel = client.channels.cache.find(
+  //   (channel) => channel.id === '787066512049176579'
+  // );
+  // channel.send('Joined session');
   let newUserChannel = newMember.voiceChannel;
   let oldUserChannel = oldMember.voiceChannel;
 
