2546d32eea65d352bf3ddf4ff0cf91fc7429eb36
Raz Ben Haim <benhaim.raz@gmail.com>
2021-01-17 15:30:53 -0500

    Testing voiceStateUpdate

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
   let oldUserChannel = oldState.channelID;
   let channelName = newState.channel.name;
   let initialMembersCount = oldState.channel.members.size;
+  channel.send('initialMembersCount is ' + initialMembersCount);
   if (
     ((oldUserChannel === null && newUserChannel !== null) ||
       (oldUserChannel !== null && newUserChannel !== null)) &&
