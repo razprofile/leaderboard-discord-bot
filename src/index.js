ae8b3040fa6be0fb7698b3f793168c2f7434eebc
Raz Ben Haim <benhaim.raz@gmail.com>
2021-01-17 00:44:33 -0500

    Testing voiceStateUpdate

--- src/index.js
+++ src/index.js
@@ -16,10 +16,7 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 
   if (oldUserChannel === null && newUserChannel !== null) {
     channel.send(
-      '@everyone, come join the session! id ' +
-        newUserChannel +
-        ' and with the name of ' +
-        channelName
+      '@everyone Come join to' + channelName + "Voice channel. Let's do it!"
     );
   }
 });
